67b0bed7a0af5566de96617cffcf3c79fb6b7b7a
Konstantin Romanov <kokonstyadev@gmail.com>
2022-09-06 18:50:26 +0700

    add go_router library

---
+++ lib/pages/details_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DetailsPage')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => context.go('/'),
+              child: const Text('Go back to home page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
